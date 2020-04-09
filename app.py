84ee5bd5310e54a880c74c91a2eb0a585ac2dca6
Matteo <matteo.ficcadenti@yahoo.com>
2020-04-09 21:16:25 +0200

    Added PostgreSQL environment variable

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -5,12 +6,11 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
 
 
 # 1. Creazione dell'applicazione Flask
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # 2. Creazione dell'ambiente API
 api = Api(app=app)
